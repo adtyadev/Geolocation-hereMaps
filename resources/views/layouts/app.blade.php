3beb9e0295bc219fb3b1d38a37d70e90d8de8bec
gitKharisma01 <akharismaw77@gmail.com>
2020-04-12 21:59:44 +0700

    create function to show codespace in list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                <a class="navbar-brand" style="color:gray" href="{{ url('/space') }}">
+                    Space
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
