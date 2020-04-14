0690b6691fd484cd21471e32788a90cbc216cb54
gitKharisma01 <akharismaw77@gmail.com>
2020-04-14 13:49:10 +0700

    add function edit and validate data codespace

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('space','SpaceController');
 Route::resource('create','SpaceController/create');
-Route::get('/browse/space', 'SpaceController@browse');
+Route::get('/browse/space', 'SpaceController@browse')->name('space.browse');
